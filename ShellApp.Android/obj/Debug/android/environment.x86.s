54bd5292721c1c2f9fbaaf821aa45284bf0ded20
ingridutz95 <gri.ingridb@gmail.com>
2021-04-14 22:47:01 +0300

    Work on the Challenges page

--- ShellApp.Android/obj/Debug/android/environment.x86.s
+++ ShellApp.Android/obj/Debug/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"afbd93a7-c6b0-4c3f-8124-327604adb5ba"
+	.asciz	"5cf1271a-4e4a-4e66-8f60-85c67bc15016"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
